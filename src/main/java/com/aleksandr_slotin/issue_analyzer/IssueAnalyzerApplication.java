94654243b7d207d8ea6af0dee239d0aa071a903b
Aleksandr Slotin <sslotin74@gmail.com>
2026-05-23 16:50:46 +0300

    base classes for project

---
+++ src/main/java/com/aleksandr_slotin/issue_analyzer/IssueAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.aleksandr_slotin.issue_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleksandr_slotin/issue_analyzer/IssueAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandr_slotin.issue_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
